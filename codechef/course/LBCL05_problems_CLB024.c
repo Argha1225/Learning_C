0b65adb712edb047ea9b030c9026fab2c1b69225
Argha1225 <arghajitdas9@gmail.com>
2024-03-19 00:59:36 +0530

    my code

---
+++ codechef/course/LBCL05_problems_CLB024.c
@@ -0,0 +1,24 @@
+// Write a program which does the following
+
+//     Declare two integer variables a and b
+//     Assign the value 23 to a and 20 to b
+//     Output the sum of a and b to the console
+
+// To sum a and b, you can print a + b, like we did with numbers previously.
+
+#include <stdio.h>
+
+int main() {
+    int a, b;
+    
+	// Assign the value 23 to a
+    a = 23;
+    
+    // Assign the value 20 to b
+    b = 20;
+    
+    // Output the sum of a and b
+    printf("%d", a + b);
+    
+    return 0;
+}
